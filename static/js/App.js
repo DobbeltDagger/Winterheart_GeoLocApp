6f547081dbfe2ce9740a0d5f568c462be966e0da
Anders Visti <andersvisti@gmail.com>
2022-12-13 16:11:29 +0100

    big cleanup from larger repo

--- static/js/App.js
+++ static/js/App.js
@@ -1,8 +1,6 @@
 import { drawHeart, loadHeart } from "./loadHeart.js";
-import { drawSupaData } from "./loadSupaBaseData.js";
 import { setupPositionFinder, getTheLocationFromDataset } from "./geoFindMe.js";
 import { closeOverlay, setupGalleryNavButtons, setupMenu } from "./menu.js";
-import { clearForm, setupMediaUploadForm } from "./forms.js";
 // import { loadSupaBaseLocations, loadSupaBaseMedia } from "../../supabase.js";
 
 
@@ -11,11 +9,8 @@ export class App {
 
   ///////////////////////////////////////
   // constructor
-  constructor(locations, media) {
-    this.supaLocations = locations;
-    this.supaMedia = media;
+  constructor() {
     this.map = null;
-    this.markers = [];
   }
 
 
@@ -37,11 +32,6 @@ export class App {
     // load data
     const heartData = await loadHeart();
 
-    // setup media overlay validation
-    // let _this = this;
-    setupMediaUploadForm();
-    this.createReloadButton(); // makes the invisble reload button!
-
     ////////////////////////////////////
     // mapbox load maps
     // let _this = this; // I DONT HAVE TO DO THIS!!!
@@ -53,25 +43,17 @@ export class App {
         e.preventDefault();
         document.querySelector('#loadingScreen').classList.add("hidden"); // remove entire loadingscreen!
 
-        // check my position
-        setupPositionFinder(heartData, _this.map);      
-        // setup menu
-        setupMenu(_this.map);
-        setupGalleryNavButtons();
-        
         // Draw all KMLs onto globe
         drawHeart(_this.map, heartData);
-        _this.markers = drawSupaData(_this.map, _this.supaLocations, _this.supaMedia);
-        console.log("_this.markers:", _this.markers);        
       })
 
       // hide loadingscreen
       document.querySelector("#loadWrapper").classList.add("hidden");
       document.querySelector("#enterWrapper").classList.add("shown");
-
     });
 
 
+    /*
     // SCaling icons on zoom!
     // from here: https://stackoverflow.com/questions/63870324/how-to-implement-mapbox-expression-inside-marker
     this.map.on('zoom', () => {
@@ -96,61 +78,9 @@ export class App {
           marker.classList.add("imageMarker");
         }
       }
-  });
+    });
+    */
 
   }
 
-
-  ///////////////////////////////////////
-  // make a button that is shown when i've saved
-  // .. and need to reload front map!
-  createReloadButton() {
-
-    const rlBtn = document.createElement("button");
-    rlBtn.innerText = "Show me on the map!";
-
-    // listen for click
-    let _this = this;
-    rlBtn.addEventListener("click", async function(e) {
-      e.preventDefault();
-
-      console.log("RELOAD THE MAP HERE!");
-      // RELOAD MAP on this CALLBACK!
-      // DELETE OLD MARKERS!
-      for (let i = 0; i < _this.markers.length; i++) { _this.markers[i].remove(); }
-      _this.markers.length = 0; // reset markers array!
-
-      // get locations and media items
-      fetch("/get-data", { method: 'GET' })
-        .then( res => res.json())
-        .then((res) => {
-          //  console.log("res:", res);
-          const locations = JSON.parse(res.Locations);
-          const mediaItems = JSON.parse(res.MediaItems);
-          // console.log("locations:", locations);
-          // console.log("mediaItems:", mediaItems);
-          _this.markers = drawSupaData(_this.map, locations, mediaItems); // redraw markers!
-          // close overlay
-          closeOverlay();
-          // ... AND then zoom to new position?
-          const loc = getTheLocationFromDataset();
-          _this.map.flyTo({
-            center: [ loc.lng, loc.lat ],
-            essential: true, // this animation prefers-reduced-motion,
-            zoom: 17 // 18
-          });
-          // Clear the Share Media form
-          clearForm();
-        })
-        .catch(err => {
-          console.error(err);
-        })
-  
-    })
-
-    // append
-    const elm = document.getElementById("reloadBtnWrapper");
-    elm.appendChild(rlBtn);
-
-  }
 }
--- static/js/index.js
+++ static/js/index.js
@@ -3,9 +3,7 @@ import { App } from "./App.js";
 
 window.addEventListener("DOMContentLoaded", function() {
 
-  console.log("index.js -> locations:", locations);
-  console.log("index.js -> media:", media);
-  const app = new App(locations, media);
+  const app = new App();
   app.init();
 
 })
